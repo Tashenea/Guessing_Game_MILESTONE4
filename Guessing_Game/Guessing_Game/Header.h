937a52d3f53ebd7eefa4d5bf5b049d91e3ada6eb
tashenea <tmb15@humboldt.edu>
2020-03-24 19:29:42 -0700

    Touching up

--- Guessing_Game/Guessing_Game/Header.h
+++ Guessing_Game/Guessing_Game/Header.h
@@ -9,14 +9,14 @@ using namespace std;
 struct BinaryTreeNode
 {
 	string data;
-	BinaryTreeNode* leftChild;
-	BinaryTreeNode* rightChild;
+	BinaryTreeNode* leftChild = NULL;
+	BinaryTreeNode* rightChild = NULL;
 };
 
 class BinaryTree
 {
 private:
-	BinaryTreeNode* root;
+	BinaryTreeNode* root = nullptr;
 
 
 public:
@@ -34,8 +34,7 @@ public:
 void BinaryTree::begin(BinaryTreeNode* tree)
 {
 	string input_1, input_2, input_3;
-	if (tree != NULL)
-
+	if (tree == NULL)
 	{
 		cout << tree->data << " ";
 		getline(cin, input_1);
@@ -70,7 +69,7 @@ void BinaryTree::begin(BinaryTreeNode* tree)
 		}
 	}
 
-	if (/*tree->leftChild != NULL*/ tree->leftChild != 0)
+	if (tree->leftChild != nullptr)
 	{
 		if (tree->leftChild->leftChild != NULL)
 		{
@@ -224,12 +223,14 @@ void IgnoreLine()
 }
 void Instructions()
 {
-	cout << "Rules Of the Game";
-	cout << "1. Player 1 and Player 2 one is a thinker and the other is a guesser";
-	cout << "2. The Thinker is the player who thinks of a person, place or thing.";
-	cout << "3. The Guesser is the player who guesses what the thinker is thinking by asking yes or no questions";
-	cout << "4. The game will continue until the guesser gives up or finds the answer.";
+
+	cout << "Rules Of the Game"  <<endl;
+	cout << "1. Player 1 and Player 2 one is a thinker and the other is a guesser" << endl;
+	cout << "2. The Thinker is the player who thinks of a person, place or thing." << endl;
+	cout << "3. The Guesser is the player who guesses what the thinker is thinking by asking yes or no questions" << endl;
+	cout << "4. The game will continue until the guesser gives up or finds the answer." << endl;
 	IgnoreLine();
+	
 }
 
 
